751b7476a71121fe5d268ed9a329db9ae714f4a1
codystack <ebuzzadvert@gmail.com>
2025-01-28 02:39:31 +0100

    footer added

--- components/footer.php
+++ components/footer.php
@@ -3,31 +3,30 @@
                 <div class="row" data-cues="slideInUp" data-duration="800">
                     <div class="col-xl-4 col-lg-9 col-md-6">
                         <div class="footer-weight">
-                            <h2 class="text-white">Subscribe Newsletter</h2>
-                            <form class="footer-form position-relative">
-                                <input type="email" class="form-control" placeholder="Enter Your Email">
-                                <button type="submit" class="default-btn">Subscribe <i class="ri-arrow-right-up-line"></i></button>
-                            </form>
+                            <a class="navbar-brand logo-brand p-0 mb-3" href="./">
+                                <img src="assets/images/bb_logo_light.svg" width="300" alt="image">
+                            </a>
+                            <p class="text-white mt-3 mb-3">Simplify your financial life today with fast, secure, and smart transactions. Download now and take control of your money, from everyday bills to digital assets!</p>
                             <ul class="social">
                                 <li><span>Follow Us:</span></li>
                                 <li>
-                                    <a href="https://www.facebook.com/" target="_blank">
+                                    <a href="https://www.facebook.com/billbuddyhq" target="_blank">
                                         <i class="ri-facebook-fill"></i>
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="https://www.twitter.com/" target="_blank">
+                                    <a href="https://www.twitter.com/billbuddyhq" target="_blank">
                                         <i class="ri-twitter-x-line"></i>
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="https://www.instagram.com/" target="_blank">
+                                    <a href="https://www.instagram.com/billbuddyhq" target="_blank">
                                         <i class="ri-instagram-line"></i>
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="https://www.linkedin.com/" target="_blank">
-                                        <i class="ri-linkedin-fill"></i>
+                                    <a href="https://wa.me/+2348103726926?text=Hello,%20Billbuddy%20I%20would%20like%20to%20reach%20out%20to%20the%20support%20team.%20" target="_blank">
+                                        <i class="ri-whatsapp-fill"></i>
                                     </a>
                                 </li>
                             </ul>
@@ -39,65 +38,51 @@
 
                             <ul class="service-link">
                                 <li>
-                                    <a href="terms-conditions.html">Terms & Conditions</a>
+                                    <a href="about">About Us</a>
                                 </li>
                                 <li>
-                                    <a href="blog.html">Blog & News</a>
+                                    <a href="faq">FAQ's</a>
                                 </li>
                                 <li>
-                                    <a href="services.html">Mobile App</a>
+                                    <a href="how-it-works">How it Works</a>
                                 </li>
                                 <li>
-                                    <a href="services.html">Why Choose Us</a>
-                                </li>
-                                <li>
-                                    <a href="pricing.html">Pricing Plan</a>
+                                    <a href="contact">Contact Us</a>
                                 </li>
                             </ul>
                         </div>
                     </div>
                     <div class="col-xl-3 col-lg-4 col-md-6">
                         <div class="footer-weight part-three ps-5">
-                            <h3 class="text-white">Our Services</h3>
+                            <h3 class="text-white">Legal</h3>
 
                             <ul class="service-link">
                                 <li>
-                                    <a href="services.html">Mobile Banking</a>
+                                    <a href="terms">Terms of Service</a>
                                 </li>
                                 <li>
-                                    <a href="services.html">Advanced Security</a>
+                                    <a href="policy">Privacy Policy</a>
                                 </li>
                                 <li>
-                                    <a href="services.html">Digital Wallet</a>
-                                </li>
-                                <li>
-                                    <a href="services.html">Budgeting Tools</a>
-                                </li>
-                                <li>
-                                    <a href="services.html">Making Transactions</a>
+                                    <a href="aml">AML Policy</a>
                                 </li>
                             </ul>
                         </div>
                     </div>
                     <div class="col-xl-3 col-lg-4 col-md-6">
                         <div class="footer-weight ps-5">
-                            <h3 class="text-white">Get In Touch</h3>
+                            <h3 class="text-white">Get in Touch</h3>
 
                             <ul class="get-touch">
                                 <li>
-                                    <img src="assets/images/svg/map.svg" alt="image">
-                                    <span class="text-white"><b>Location:</b></span>
-                                    <a href="https://www.google.com/maps/search/18+Tanglewood+Lane+Gulfport/@30.4216847,-89.1511032,12z/data=!3m1!4b1?entry=ttu" target="_blank">18 Tanglewood Lane Gulfport</a>
-                                </li>
-                                <li>
                                     <img class="phone" src="assets/images/svg/mail.svg" alt="image">
-                                    <span class="text-white"><b>Email:</b></span>
-                                    <a href="../../cdn-cgi/l/email-protection.html#3c54595050537c5a55524853125f5351"><span class="__cf_email__" data-cfemail="375f525b5b5877515e5943581954585a">[email&#160;protected]</span></a>
+                                    <span class="text-white"><b>Email</b></span>
+                                    <a href="mailto:hello@billbuddy.africa">hello@billbuddy.africa</a>
                                 </li>
                                 <li>
                                     <img class="phone" src="assets/images/svg/phone.svg" alt="image">
-                                    <span class="text-white"><b>Phone:</b></span>
-                                    <a href="tel:0018085550148">+001 (808) 5550148</a>
+                                    <span class="text-white"><b>Phone</b></span>
+                                    <a href="tel:+2348103726926">+234 810 3726 926</a>
                                 </li>
                             </ul>
                         </div>
@@ -106,30 +91,13 @@
             </div>
         </div>
         
-        
+
         <div class="copyright-area bg-color-000000">
             <div class="container">
                 <div class="copyright-border">
                     <div class="row g-4 align-items-center">
-                        <div class="col-lg-4 col-md-6">
-                            <p>© <span>Finto</span> Is Proudly Owned By <a href="https://hibootstrap.com/" target="_blank">HiBootstrap</a></p>
-                        </div>
-                        <div class="col-lg-4 col-md-6">
-                            <div class="copy-image" data-cues="zoomIn" data-duration="700">
-                                <a href="index.html" class="d-block">
-                                    <img src="assets/images/white-logo.png" alt="image">
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-lg-4 col-md-12">
-                            <ul>
-                                <li>
-                                    <a href="privacy-policy.html">Privacy Policy</a>
-                                </li>
-                                <li>
-                                    <a href="terms-conditions.html">Terms & Conditions</a>
-                                </li>
-                            </ul>
+                        <div class="col-lg-6 col-md-6 mx-auto text-center">
+                            <p>© <script>document.write(new Date().getFullYear());</script> Billbuddy. All Rights Reserved.</p>
                         </div>
                     </div>
                 </div>
