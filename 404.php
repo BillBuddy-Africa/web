83c6812dd17a098565533f755ca9b4aef79e248d
codystack <ebuzzadvert@gmail.com>
2025-01-28 03:01:37 +0100

    404 added

---
+++ 404.php
@@ -0,0 +1,21 @@
+<?php include "./components/header.php"; ?>
+        
+        <div class="error-area ptb-150">
+            <div class="container">
+                <div class="error-content-wrap">
+                    <img class="error-image" src="assets/images/error-image.png" width="400" alt="image">
+                    <p class="text-black">We'r sorry, the page you requested could not be found Please go back to the homepage</p>
+
+                    <a href="./" class="default-btn">Go To Homepage</a>
+                </div>
+            </div>
+        </div>
+
+        <script src="assets/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/js/swiper-bundle.min.js"></script>
+        <script src="assets/js/fslightbox.min.js"></script>
+        <script src="assets/js/smooth-scroll.js"></script>
+        <script src="assets/js/scrollCue.min.js"></script>
+        <script src="assets/js/script.js"></script>
+    </body>
+</html>
